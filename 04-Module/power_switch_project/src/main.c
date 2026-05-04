d77ceb2bb8a518b5961d7aee80da7b56c58b1ce6
EhabMagdyy <ehabmagdy0044@gmail.com>
2026-05-04 17:08:27 +0300

    Power Switch Driver/Module

---
+++ 04-Module/power_switch_project/src/main.c
@@ -0,0 +1,24 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include "drivers/power_switch.h"
+
+#define PWR_SW_NODE DT_NODELABEL(power_switches)
+
+int main(void)
+{
+    const struct device *pwr = DEVICE_DT_GET(PWR_SW_NODE);
+
+    if (!device_is_ready(pwr)) {
+        printk("Power switch not ready!\n");
+        return -1;
+    }
+
+    while (1) {
+        power_switch_on(pwr);
+        k_sleep(K_SECONDS(2));
+        power_switch_off(pwr);
+        k_sleep(K_SECONDS(2));
+    }
+    return 0;
+}
